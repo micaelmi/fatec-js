c4792c8bb422a040c3645eacfc50c846d244172c
garradefogo124 <micaelmiranda124@gmail.com>
2022-09-16 11:36:43 -0300

    Form com validações de campos e uso de API

---
+++ Aula3/js/general.js
@@ -0,0 +1,81 @@
+
+function dateValidation(date) {
+    let day, month, year, hour, minute;
+    let size = date.length;
+    if (date === null) {
+        return false;
+    }
+    if (size === 0 || (size != 10 && size != 16)) {
+        return false;
+    }
+
+    date = date.replaceAll("-", "/")
+    vetorDate = date.split("/");
+    if (vetorDate[0].length === 4) {
+        year = vetorDate[0]
+        month = vetorDate[1];
+        day = vetorDate[2];
+    } else {
+        day = vetorDate[0];
+        month = vetorDate[1];
+        year = vetorDate[2];
+    }
+
+    if (day.length > 2) {
+        let vetorDay = day.split("T")
+        day = vetorDay[0];
+        let vetorHour = vetorDay[1].split(":")
+        hour = vetorHour[0]
+        minute = vetorHour[1]
+    }
+
+    // Validação de mês
+    if (month < 1 || month > 12) {
+        console.log("Mês inválido")
+        return false;
+    }
+
+    // Validação de Ano
+    if (year < 1900) {
+        console.log("Ano inválido")
+        return false;
+    }
+
+    // meses com 31 dias
+    if (month == 1 || month == 3 || month == 5 ||
+        month == 7 || month == 8 || month == 10 || month == 12) {
+        if (day <= 31) {
+            return true;
+        } else {
+            return false;
+        }
+        // meses com 30 dias
+    } else if (month != 2) {
+        if (day <= 30) {
+            return true;
+        } else {
+            return false;
+        }
+        // fevereiro
+    } else {
+        if ((year % 400 === 0) || (year % 100 !== 0) && (year % 4 === 0)) {
+            // fevereiro com 29 dias
+            if (day <= 29) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            // fevereiro com 28 dias
+            if (day <= 28) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
+
+function emptyFieldValidation(value) {
+    return value.length !== 0 ? false : true;
+}
--- Aula3/js/index.js
+++ Aula3/js/index.js
@@ -1,13 +1,48 @@
 let button = document.getElementById('button');
 let avatar = document.getElementById('avatar');
+let error = document.getElementById('error');
+let error2 = document.getElementById('error2');
+let error3 = document.getElementById('error3');
+let myDate = document.getElementById('date');
+let myDate2 = document.getElementById('date2');
+let myName = document.getElementById('name');
+let allValid = true;
+let errorMessage = "Data incorreta"
+
 button.addEventListener('click', verify);
+myDate.addEventListener('blur', function () {
+    verifyDate(myDate.value, error, errorMessage)
+});
+myDate2.addEventListener('blur', function () {
+    verifyDate(myDate2.value, error2, errorMessage)
+});
+myName.addEventListener('blur', function () {
+    verifyEmptyField(myName.value, error3);
+})
+
+function verifyDate(value, errorField, message) {
+    if (!dateValidation(value)) {
+        errorField.textContent = message;
+    } else {
+        errorField.textContent = "";
+    }
+}
+
+function verifyEmptyField(value, errorField) {
+
+    if (emptyFieldValidation(value)) {
+        errorField.textContent = "Preencha esse campo"
+    } else {
+        errorField.textContent = ""
+    }
+}
 
 function verify() {
-    let myDate = document.getElementById('date')
-    if (dateValidation(myDate.value)) {
-        Swal.fire("Esta data é válida")
-        avatar.src = `https://avatars.dicebear.com/api/bottts/${myDate.value}.svg`
-    } else {
-        Swal.fire("Esta data não é válida")
+    verifyDate(myDate.value, error, errorMessage);
+    verifyDate(myDate2.value, error2, errorMessage);
+    verifyEmptyField(myName.value, error3);
+
+    if (error.textContent == "" && error2.textContent == "" && error3.textContent == "") {
+        avatar.src = `https://avatars.dicebear.com/api/bottts/${myDate.value}${myDate2.value}${myName.value}.svg`;
     }
 }
